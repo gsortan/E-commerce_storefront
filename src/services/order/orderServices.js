26b9e784df6b93e2e2ecef92d3f764206650969e
Gordon Tan <gordonsortan@gmail.com>
2026-02-08 01:52:38 -0500

    Changing revenue aggregation function

--- src/services/order/orderServices.js
+++ src/services/order/orderServices.js
@@ -32,20 +32,15 @@ export async function getLowestOrderYear() {
   return lowestYearOrder.createdAt.getFullYear();
 }
 
-export async function getRevenueToday()
-{
-const [orders, totalCount] = await prisma.$transaction([
-    prisma.order.findMany({
-      where: { userId: uid, createdAt: createdAtFilter },
-      orderBy: { createdAt: "desc" },
-      skip: skipVal,
-      take: takeVal,
-      include: {
-        items: { include: { product: true } },
-      },
-    }),
-    prisma.order.count(),
-  ]);
+export async function getTotalRevenue() {
+
+  const resultRevenue = await prisma.order.aggregate({
+    _sum: {
+      totalAmount: true,
+    },
+  });
+
+  return resultRevenue._sum.totalAmount.toString();
 }
 
 export async function getOrders(skipVal, takeVal, uid, dateStart, dateEnd) {
@@ -65,12 +60,12 @@ export async function getOrders(skipVal, takeVal, uid, dateStart, dateEnd) {
         items: { include: { product: true } },
       },
     }),
-   prisma.order.count({
-    where: {
-      userId: uid,
-      createdAt: createdAtFilter,
-    },
-      }),
+    prisma.order.count({
+      where: {
+        userId: uid,
+        createdAt: createdAtFilter,
+      },
+    }),
   ]);
 
   console.log(orders[1].createdAt.getFullYear());
